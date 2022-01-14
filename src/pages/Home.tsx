1c8f4db73ebc420d6f3bbc63a7706df69890ef5c
Pedro <pedrodavila.bovolin@gmail.com>
2022-01-14 01:48:20 -0300

    Autenticaçao com Google adicionada

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,28 @@
+import { useNavigate } from 'react-router-dom'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
 import { Button } from '../components/Button'
 
+import { useAuth } from '../hooks/useAuth'
+
 import '../styles/auth.scss'
 
 export function Home() {
+
+  const navigate = useNavigate()
+  const { user, signInWithGoogle } = useAuth()
+
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle()
+    }
+
+    navigate('/rooms/new')
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -17,14 +33,14 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do google" />
             Crie sua sala com o google
           </button>
           <div className="separator">Ou entre em uma sala</div>
           <form>
-            <input 
-              type="text" 
+            <input
+              type="text"
               placeholder="Digite o código da sala"
             />
             <Button type="submit">Entrar na sala</Button>
